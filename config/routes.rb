aaf3dea409f0735c5c66480f5fcaf3b23f54b320
Andrej Kolar <andrej.kolar@me.com>
2013-11-14 15:23:47 +0100

    Created calendar controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ProjectCalendar::Application.routes.draw do
+  get "calendar/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
