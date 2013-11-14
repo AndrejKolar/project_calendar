95d2218a2a4cd64f5df8aac3f37206f7e7ac17b4
Andrej Kolar <andrej.kolar@me.com>
2013-11-14 15:28:40 +0100

    Root set to calendar#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 ProjectCalendar::Application.routes.draw do
+
   get "calendar/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'calendar#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -40,7 +41,7 @@ ProjectCalendar::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
