bc7c5867ee162572109a150c320f2bb0667ec791
Andrej Kolar <andrej.kolar@me.com>
2013-11-20 15:42:36 +0100

    Creating events works

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,74 +1,109 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
-
   # GET /events
-  # GET /events.json
+  # GET /events.xml
   def index
-    @events = Event.all
+    # full_calendar will hit the index method with query parameters
+    # 'start' and 'end' in order to filter the results for the
+    # appropriate month/week/day.  It should be possiblt to change
+    # this to be starts_at and ends_at to match rails conventions.
+    # I'll eventually do that to make the demo a little cleaner.
+    @events = Event.scoped
+    @events = @events.after(params['start']) if (params['start'])
+    @events = @events.before(params['end']) if (params['end'])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @events }
+      format.js  { render :json => @events }
+    end
   end
 
   # GET /events/1
-  # GET /events/1.json
+  # GET /events/1.xml
   def show
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @event }
+      format.js { render :json => @event.to_json }
+    end
   end
 
   # GET /events/new
+  # GET /events/new.xml
   def new
     @event = Event.new
+    # @event.starts_at = params[:start]
+    # @event.ends_at = params[:end]
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @event }
+    end
   end
 
   # GET /events/1/edit
   def edit
+    @event = Event.find(params[:id])
   end
 
   # POST /events
-  # POST /events.json
+  # POST /events.xml
   def create
     @event = Event.new(event_params)
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @event }
+        format.html { redirect_to(@event, :notice => 'Event was successfully created.') }
+        format.xml  { render :xml => @event, :status => :created, :location => @event }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
+  # PUT /events/1
+  # PUT /events/1.xml
+  # PUT /events/1.js
+  # when we drag an event on the calendar (from day to day on the month view, or stretching
+  # it on the week or day view), this method will be called to update the values.
+  # viv la REST!
   def update
+    @event = Event.find(params[:id])
+
     respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { head :no_content }
+      if @event.update_attributes(event_params)
+        format.html { redirect_to(@event, :notice => 'Event was successfully updated.') }
+        format.xml  { head :ok }
+        format.js { head :ok}
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
+        format.js  { render :js => @event.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
+  # DELETE /events/1.xml
   def destroy
+    @event = Event.find(params[:id])
     @event.destroy
+
     respond_to do |format|
-      format.html { redirect_to events_url }
-      format.json { head :no_content }
+      format.html { redirect_to(events_url) }
+      format.xml  { head :ok }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+private
+    # Using a private method to encapsulate the permissible parameters
+    # is just a good pattern since you'll be able to reuse the same
+    # permit list between create and update. Also, you can specialize
+    # this method with per-user checking of permissible attributes.
     def event_params
-      params[:event]
+      params.require(:event).permit(:title, :starts_at, :ends_at, :all_day, :description)
     end
 end
