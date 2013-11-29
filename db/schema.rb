d5f5380316a19e6e032c53641bda168ec87826cb
Andrej Kolar <andrej.kolar@me.com>
2013-11-29 16:19:21 +0100

    Added user events association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129144950) do
+ActiveRecord::Schema.define(version: 20131129151825) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20131129144950) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "color"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
