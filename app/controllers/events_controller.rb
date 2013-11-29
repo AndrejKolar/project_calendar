416d988361b5b5b7437179285c17bfca8afe4205
Andrej Kolar <andrej.kolar@me.com>
2013-11-29 16:29:28 +0100

    Saving events for specific users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,9 +7,10 @@ class EventsController < ApplicationController
     # appropriate month/week/day.  It should be possiblt to change
     # this to be starts_at and ends_at to match rails conventions.
     # I'll eventually do that to make the demo a little cleaner.
-    @events = Event.scoped
-    @events = @events.after(params['start']) if (params['start'])
-    @events = @events.before(params['end']) if (params['end'])
+
+    @events = current_user.events
+    # @events = @events.after(params['start']) if (params['start'])
+    #@events = @events.before(params['end']) if (params['end'])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -52,6 +53,7 @@ class EventsController < ApplicationController
   # POST /events.xml
   def create
     @event = Event.new(event_params)
+    @event.user_id = current_user.id
 
     respond_to do |format|
       if @event.save
