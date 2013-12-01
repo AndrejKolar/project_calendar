30563bb3e75bcaf8b6098fab5f5ec49abf8023e1
Andrej Kolar <andrej.kolar@me.com>
2013-12-01 17:15:38 +0100

    Showing users on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+      format.js  { render :json => @users }
+    end
   end
 end
