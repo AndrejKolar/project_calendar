fd642fd1e5934a72f1bcc8c59986a5fa5006074c
Andrej Kolar <andrej.kolar@me.com>
2013-12-02 15:18:25 +0100

    User redirection works

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+      if current_user && current_user.admin?
+        redirect_to :controller=>'dashboard', :action => 'index'
+      else
+        redirect_to :controller=>'calendar', :action => 'index'
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ProjectCalendar::Application.routes.draw do
+  get "welcome/index"
   get "dashboard/index"
   devise_for :users
   resources :events
 
   get "calendar/index"
 
-  root 'calendar#index'
+  root 'welcome#index'
 end
