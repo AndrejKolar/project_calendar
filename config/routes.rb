ed10da514e34a15ddca51f5868f290a55a573c49
Andrej Kolar <andrej.kolar@me.com>
2013-12-02 14:42:15 +0100

    Showing calendar on calendar and events controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ProjectCalendar::Application.routes.draw do
   devise_for :users
   resources :events
 
-  get "calendar/index", as: 'calendar'
+  get "calendar/index"
 
   root 'calendar#index'
 end
