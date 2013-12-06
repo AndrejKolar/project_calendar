14148b1439bde487338251583536a9b082b78e8a
Andrej Kolar <andrej.kolar@me.com>
2013-12-06 16:40:37 +0100

    Sortable report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,12 @@
 class ReportsController < ApplicationController
+  handles_sortable_columns
+
   def index
   end
 
   def get
+    # User.search(params[:search]).order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
     @user = User.find(params[:user_id])
+    @events = @user.events.order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
   end
 end
