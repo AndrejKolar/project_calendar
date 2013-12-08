ac51dda4a496bcb0f25e38b0e33cac3344e450c6
Andrej Kolar <andrej.kolar@me.com>
2013-12-08 16:57:39 +0100

    Refactorings

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,16 +5,17 @@ class ReportsController < ApplicationController
   end
 
   def get
+    @user = User.find(params[:user_id])
+
+    # date ranges
     @beginning_current = params[:beginning_of_month].try(:to_date) || Date.current.beginning_of_month
     @date_range = (@beginning_current.beginning_of_month..@beginning_current.end_of_month)
-
-    @beginning_next     = (@beginning_current + 1.month).beginning_of_month.to_s
+    @beginning_next = (@beginning_current + 1.month).beginning_of_month.to_s
     @beginning_previous = (@beginning_current - 1.month).beginning_of_month.to_s
 
-    @user = User.find(params[:user_id])
-    @events = @user.events.order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
-
-    @current_month_events = @events.where(starts_at: @date_range)
+    # get events
+    @events = @user.events.where(starts_at: @date_range)
+    @events = @events.order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
 
     respond_to do |format|
       format.html # get.html.erb
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,6 @@ class Event < ActiveRecord::Base
   scope :before, lambda {|end_time| {:conditions => ["ends_at < ?", Event.format_date(end_time)] }}
   scope :after, lambda {|start_time| {:conditions => ["starts_at > ?", Event.format_date(start_time)] }}
 
-  # need to override the json view to return what full_calendar is expecting.
-  # http://arshaw.com/fullcalendar/docs/event_data/Event_Object/
   def as_json(options = {})
     {
       :id => self.id,
@@ -19,7 +17,6 @@ class Event < ActiveRecord::Base
       :recurring => false,
       :url => Rails.application.routes.url_helpers.event_path(id)
     }
-
   end
 
   def self.format_date(date_time)
