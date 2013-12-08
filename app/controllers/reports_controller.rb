81d8c780474a2d296abc7e764c890f24ac8479a3
Andrej Kolar <andrej.kolar@me.com>
2013-12-08 16:33:11 +0100

    link_to sending begining of month as params

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,8 +5,19 @@ class ReportsController < ApplicationController
   end
 
   def get
-    # User.search(params[:search]).order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
+    @beginning_current = params[:beginning_of_month].try(:to_date) || Date.current.beginning_of_month
+    @date_range = (@beginning_current.beginning_of_month..@beginning_current.end_of_month)
+
+    @beginning_next     = (@beginning_current + 1.month).beginning_of_month.to_s
+    @beginning_previous = (@beginning_current - 1.month).beginning_of_month.to_s
+
+    @current_time = Date.current
     @user = User.find(params[:user_id])
     @events = @user.events.order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
+
+    respond_to do |format|
+      format.html # get.html.erb
+      format.json { render json: @events }
+    end
   end
 end
