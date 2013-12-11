60d93702535156cd8b66a4dc481e475188bdc522
Andrej Kolar <andrej.kolar@me.com>
2013-12-11 15:20:25 +0100

    Show/hide finished at project for finished checkbox

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-
- $('#form_id').hide(); //Initially form wil be hidden.
-
-  $('#button_id').click(function() {
-   $('#form_id').show();//Form shows on button click
-
-   });
+  $('#box').change(function() {
+      $('#div').toggle(this.checked);
+  });
  });
+
