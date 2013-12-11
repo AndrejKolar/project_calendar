b590707fd6617c0d18bb22103d8767ffda4eb361
Andrej Kolar <andrej.kolar@me.com>
2013-12-11 14:09:20 +0100

    Added total hours to projects page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,8 @@ module ProjectsHelper
     user_id_array = project.events.pluck(:user_id)
     User.where(id: user_id_array)
   end
+
+  def total_time_for_project(project)
+    project.events.sum(:total_hours)
+  end
 end
