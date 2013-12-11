fc8bce509a73fb8abb5f373751c4218f0f65ff9a
Andrej Kolar <andrej.kolar@me.com>
2013-12-11 14:20:49 +0100

    Setting started at for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :finished)
+      params.require(:project).permit(:name, :finished, :started_at, :finished_at)
     end
 end
