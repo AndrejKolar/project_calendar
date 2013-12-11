0587aa0435c17187bb50abfbad5980b82fc7a80d
Andrej Kolar <andrej.kolar@me.com>
2013-12-11 12:41:11 +0100

    Added project_id to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209115759) do
+ActiveRecord::Schema.define(version: 20131211113543) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131209115759) do
     t.string   "color"
     t.integer  "user_id"
     t.integer  "total_hours"
+    t.integer  "project_id"
   end
 
   create_table "projects", force: true do |t|
