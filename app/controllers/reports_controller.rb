1b8e2550f136cf83c19196ede45efb41538eeacb
Andrej Kolar <andrej.kolar@me.com>
2013-12-12 17:23:33 +0100

    Implemented sort on the report table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,9 +10,16 @@ class ReportsController < ApplicationController
     @beginning_next = (@beginning_current + 1.month).beginning_of_month.to_s
     @beginning_previous = (@beginning_current - 1.month).beginning_of_month.to_s
 
-    # get events
+    # events
     @events = @user.events.where(starts_at: @date_range)
     @events = @events.order(sortable_column_order).paginate(:per_page => 50, :page => params[:page])
+
+    # time
+    @total_time = @events.sum(:total_hours)
+
+    # projects
+    project_ids = @events.pluck(:project_id)
+    @projects = Project.where(id: project_ids)
 
     respond_to do |format|
       format.html
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,5 @@
 module ReportsHelper
+  def time_for_project(project, user, span)
+    Event.where(starts_at: span, user: user, project: project).sum(:total_hours)
+  end
 end
